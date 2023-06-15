11346c574ad11530a840eba0a4c61ade3def9fd7
Samuel260107 <130668952+Samuel260107@users.noreply.github.com>
2023-06-15 09:00:50 -0300

    commit do samuel

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+        if(isset($_POST["submit"])){
+        $comentario = $_POST["texto"];
+
+        //Saída
+        echo "<br><br>";
+        echo "<h2>" .$comentario. "</h2>";
+        }
+?>
